7958236869ef15c2caba13a4c42e5f88d2d7386e
jorgezm238 <wrn47806@educastur.es>
2024-12-09 16:11:25 +0100

    ggs

---
+++ PREPARACION_JS/main.js
@@ -0,0 +1,19 @@
+/*Crea una aplicación con las siguientes clases:
+
+Clase Estudiante:
+Nombre
+Nota
+
+Clase Resultado:
+Nombre
+Estado (Aprobado o Suspendido)
+
+Requisitos:
+AÑADIR: El usuario podrá introducir el nombre y la nota de varios estudiantes.
+MODIFICAR: El usuario podrá modificar el nombre y/o la nota de un estudiante ya dado de alta.
+CALCULAR: Determina si cada estudiante está aprobado o suspendido:
+Aprobado: Nota mayor o igual a 5.
+Suspendido: Nota menor a 5.
+
+Muestra una lista con el nombre de cada estudiante y su estado (Aprobado o Suspendido).
+Guarda en la clase Resultado los nombres y los estados de los estudiantes que estén suspendidos. */
