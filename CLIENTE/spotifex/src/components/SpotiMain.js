e8cf4d2a7cd1a047daa4df1ee2b1453287fa0a70
Jorge Zardain Monforte <wrn47806@educastur.es>
2025-03-12 09:11:32 +0100

    spotify

---
+++ CLIENTE/spotifex/src/components/SpotiMain.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+import ListadoCanciones from './ListadoCanciones';
+import FiltradoCanciones from './FiltradoCanciones';
+import MasPopular from './MasPopular';
+
+function SpotiMain() {
+  const [canciones, setCanciones] = useState([]);
+
+  // Función asíncrona para obtener las canciones del JSON hacer siempre para obtener los datos del json
+  useEffect(() => {
+                        async function fetchCanciones() {
+                          try {
+                              const response = await fetch('/json/Spotify.json');
+                              const data = await response.json();
+                              setCanciones(data);
+                          } catch (error) {
+                              console.error('Error al obtener las canciones:', error);
+                          }
+                        }
+
+                        fetchCanciones();
+                }, []
+            );
+/*hasta aqui(el useEffect)*/
+  return (
+    <div>
+      <h1>SpotifEx</h1>
+      <ListadoCanciones canciones={canciones} />
+      <FiltradoCanciones canciones={canciones} />
+      <MasPopular canciones={canciones} />
+    </div>
+  );
+}
+
+export default SpotiMain;
